dab599c0ea38ba99bcb1a92bb309dce7558712d6
Mathieu <emd4812@umoncton.ca>
2025-04-14 21:15:58 -0300

    fait Qsuite pour une pratique d'examen (proggramation dynamic)

---
+++ Qsuite.cpp
@@ -0,0 +1,29 @@
+//https://pydefis.callicode.fr/defis/QSuite/txt
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    vector<int> memo(2000);
+    // fill le memo
+    memo[0] = 1; 
+    memo[1] = 1; 
+    memo[2] = 1; 
+
+    int a , b;
+    cin >> a >> b;
+
+    int somme = 0;
+    // fait le tableaux pour [3,j]
+    for (int i = 3; i <= b; ++i){
+        //Q(n)=Q(n-Q(n-1))+Q(n-Q(n-2))
+        memo[i] = memo[ i - memo[i-1]  ] + memo[i-memo[i-2]];
+
+        if (i>= a && i<= b){
+
+            somme += memo[i];
+        }
+    }
+    cout << somme << endl;
+}
