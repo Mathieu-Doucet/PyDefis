c423286a0fcec1816456c0e9905d13f7d76251d2
Mathieudoucet000@gmail.com <Mathieudoucet000@gmail.com>
2025-05-19 20:04:30 -0300

    Ajoutait des nombres deux fois si le etait divisable par 3 ET 5 (ex: 15) corriger

---
+++ Rigolade/SpymasterBomb.cpp
@@ -0,0 +1,26 @@
+//https://pydefis.callicode.fr/defis/SpymasterBomb/txt
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // /*
+    int nombre = 1435 ; 
+    int code = 0;
+    int i = 3;
+
+    // strictement inferieur 
+
+    while(i<nombre){
+
+        if(i % 3 == 0 || i % 5 == 0){
+            code += i;
+        }
+        i++;
+    }
+    
+    cout << code << endl;
+    // */
+
+   
+}
